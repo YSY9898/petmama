f3164279ba91a36521d48c95659b603872766aba
zewnt <zewnt@Eunhee>
2023-12-14 17:32:13 +0900

    board up

--- petmama/src/main/java/kr/board/dao/BoardDAO.java
+++ petmama/src/main/java/kr/board/dao/BoardDAO.java
@@ -6,11 +6,13 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import kr.board.vo.BoardFavVO;
+import kr.board.vo.BoardReplyVO;
 import kr.board.vo.BoardVO;
-import kr.member.vo.MemberVO;
 import kr.util.DBUtil;
+import kr.util.DurationFromNow;
 import kr.util.StringUtil;
-
+   
 public class BoardDAO {
 	//싱글턴 패턴
 	private static BoardDAO instance = new BoardDAO();
@@ -29,16 +31,15 @@ public class BoardDAO {
 
 		try {
 			conn = DBUtil.getConnection();
-			sql = "INSERT INTO board (board_num,board_type,title,content,filename,ip,mem_num,mem_nickname) VALUES (board_seq.nextval,0,?,?,?,?,?,?)";
-			System.out.println(sql);
+			sql = "INSERT INTO board (board_num,title,content,filename,ip,mem_num) VALUES (board_seq.nextval,?,?,?,?,?)";
+			//System.out.println(sql);
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, board.getTitle());
 			pstmt.setString(2, board.getContent());
 			pstmt.setString(3, board.getFilename());
 			pstmt.setString(4, board.getIp());
 			pstmt.setInt(5, board.getMem_num());
-			pstmt.setString(6, board.getMem_nickname());
-			
+			 
 			pstmt.executeUpdate();
 			
 		}catch(Exception e) {
@@ -68,7 +69,7 @@ public class BoardDAO {
 				else if(keyfield.equals("3")) sub_sql += "WHERE content LIKE ?";
 			}
 			//SQL문 작성
-			sql = "SELECT COUNT(*) FROM board JOIN member USING(mem_num)" + sub_sql;
+			sql = "SELECT COUNT(*) FROM board JOIN member USING(mem_num) LEFT OUTER JOIN member_detail USING(mem_num) " + sub_sql;
 			//PreparedStatement 객체 생성
 			pstmt = conn.prepareStatement(sql);
 			if(keyword!=null && !"".equals(keyword)) { //객체가 생성되었는데 비어있을 수도 있어서 조건 검사
@@ -106,10 +107,10 @@ public class BoardDAO {
 				//검색 처리
 				if(keyfield.equals("1")) sub_sql += "WHERE title LIKE ?";
 				else if(keyfield.equals("2")) sub_sql += "WHERE mem_nickname LIKE ?";
-				else if(keyfield.equals("3")) sub_sql += "WHERE content LIKE ?";
+				else if(keyfield.equals("3")) sub_sql += "WHERE content LIKE ?";	
 			}
 			//SQL문 작성
-			sql = "SELECT * FROM (SELECT a.*, rownum rnum FROM (SELECT * FROM board JOIN member USING(mem_num) " + sub_sql + " ORDER BY board_num DESC)a) WHERE rnum >= ? AND rnum <= ?";
+			sql = "SELECT * FROM (SELECT a.*, rownum rnum FROM (SELECT * FROM board JOIN member USING(mem_num) LEFT OUTER JOIN member_detail USING(mem_num) " + sub_sql + " ORDER BY board_num DESC)a) WHERE rnum >= ? AND rnum <= ?";
 			//PreparedStatement 객체 생성
 			pstmt = conn.prepareStatement(sql);
 			
@@ -131,6 +132,7 @@ public class BoardDAO {
 				board.setHit(rs.getInt("hit"));
 				board.setReg_date(rs.getDate("reg_date"));
 				board.setMem_nickname(rs.getString("mem_nickname"));
+				board.setMem_id(rs.getString("mem_id"));
 				
 				list.add(board);
 			}
@@ -144,23 +146,425 @@ public class BoardDAO {
 		return list;
 	}
 	//글 상세
+	public BoardVO getBoard(int board_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardVO board = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "SELECT * FROM board JOIN member USING(mem_num) LEFT JOIN member_detail USING(mem_num) LEFT JOIN pet_detail USING(mem_num) WHERE board_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, board_num);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				board = new BoardVO();
+				board.setBoard_num(rs.getInt("board_num"));
+				board.setTitle(rs.getString("title"));
+				board.setContent(rs.getString("content"));
+				board.setHit(rs.getInt("hit"));
+				board.setReg_date(rs.getDate("reg_date"));
+				board.setModify_date(rs.getDate("modify_date"));
+				board.setFilename(rs.getString("filename"));
+				board.setMem_num(rs.getInt("mem_num"));
+				board.setMem_nickname(rs.getString("mem_nickname"));
+				board.setPet_photo(rs.getString("pet_photo"));
+			}
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return board;
+	}
 	//조회수 증가
+	public void updateReadcount(int board_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "UPDATE board SET hit=hit+1 WHERE board_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, board_num);
+			pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
 	//파일 삭제
+	public void deleteFile(int board_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "UPDATE board SET filename='' WHERE board_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, board_num);
+			pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
 	//글 수정
+	public void updateBoard(BoardVO board)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		String sub_sql = "";
+		int cnt = 0;
+		
+		try {
+			conn = DBUtil.getConnection();
+			if(board.getFilename()!=null) {
+				sub_sql += ",filename=?";
+			}
+			sql = "UPDATE board SET title=?,content=?,modify_date=SYSDATE,ip=?" + sub_sql + " WHERE board_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(++cnt, board.getTitle());
+			pstmt.setString(++cnt, board.getContent());
+			pstmt.setString(++cnt, board.getIp());
+			if(board.getFilename()!=null) {
+				pstmt.setString(++cnt, board.getFilename());
+			}			
+			pstmt.setInt(++cnt, board.getBoard_num());
+			pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
 	//글 삭제
+	public void deleteBoard(int board_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		PreparedStatement pstmt2 = null;
+		PreparedStatement pstmt3 = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			conn.setAutoCommit(false);
+			
+			//좋아요 삭제
+			sql = "DELETE FROM board_fav WHERE board_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, board_num);
+			pstmt.executeUpdate();
+			
+			//댓글 삭제
+			sql = "DELETE FROM board_reply WHERE board_num=?";
+			pstmt2 = conn.prepareStatement(sql);
+			pstmt2.setInt(1, board_num);
+			pstmt2.executeUpdate();
+			
+			//부모글 삭제
+			sql = "DELETE FROM board WHERE board_num=?";
+			pstmt3 = conn.prepareStatement(sql);
+			pstmt3.setInt(1, board_num);
+			pstmt3.executeUpdate();
+			
+			conn.commit();
+			
+		}catch(Exception e) {
+			conn.rollback();
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt3, conn);
+		}
+	}
 		
 	//좋아요 등록
+	public void insertFav(BoardFavVO favVO)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "INSERT INTO board_fav (board_num,mem_num) VALUES (?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, favVO.getBoard_num());
+			pstmt.setInt(2, favVO.getMem_num());
+			pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
 	//좋아요 개수
+	public int selectFavConut(int board_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		int count = 0;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "SELECT COUNT(*) FROM board_fav WHERE board_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, board_num);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				count = rs.getInt(1);
+			}
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return count;
+	}
 	//회원번호와 게시물 번호를 이용한 좋아요 정보 (좋아요 상세 정보)
+	public BoardFavVO selectFav(BoardFavVO favVO)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardFavVO fav = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "SELECT * FROM board_fav WHERE board_num=? AND mem_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, favVO.getBoard_num());
+			pstmt.setInt(2, favVO.getMem_num());
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				fav = new BoardFavVO();
+				fav.setBoard_num(rs.getInt("board_num"));
+				fav.setMem_num(rs.getInt("mem_num"));
+			}
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		
+		return fav;
+	}
 	//좋아요 삭제
+	public void deleteFav(BoardFavVO favVO)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "DELETE FROM board_fav WHERE board_num=? AND mem_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, favVO.getBoard_num());
+			pstmt.setInt(2, favVO.getMem_num());
+			pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
 	//내가 선택한 좋아요 목록
+	public List<BoardVO> getListBoardFav(int start,int end,int mem_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		List<BoardVO> list = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "SELECT * FROM (SELECT a.*, rownum rnum FROM (SELECT * FROM board JOIN member USING(mem_num) JOIN member_detail USING(mem_num) JOIN board_fav f USING(board_num) WHERE f.mem_num=? ORDER BY board_num DESC)a)WHERE rnum >=? AND rnum <=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, mem_num);
+			pstmt.setInt(2, start);
+			pstmt.setInt(3, end);
+			
+			rs = pstmt.executeQuery();
+			list = new ArrayList<BoardVO>();
+			while(rs.next()) {
+				BoardVO board = new BoardVO();
+				board.setBoard_num(rs.getInt("board_num"));
+				board.setTitle(StringUtil.useNoHtml(rs.getString("title")));
+				board.setReg_date(rs.getDate("reg_date"));
+				board.setMem_nickname(rs.getString("mem_nickname"));
+				
+				list.add(board);
+				
+			}
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		
+		return list;
+	}
 		
 	//댓글 등록
+	public void insertReplyBoard(BoardReplyVO boardReply)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "INSERT INTO board_reply (re_num,re_content,re_ip,mem_num,board_num) VALUES (board_reply_seq.nextval,?,?,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, boardReply.getRe_content());
+			pstmt.setString(2, boardReply.getRe_ip());
+			pstmt.setInt(3, boardReply.getMem_num());
+			pstmt.setInt(4, boardReply.getBoard_num());
+			pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
 	//댓글 개수
+	public int getReplyBoardCount(int board_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		int count = 0;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "SELECT COUNT(*) FROM board_reply JOIN member USING(mem_num) WHERE board_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, board_num);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				count = rs.getInt(1);
+			}
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		
+		return count;
+	}
 	//댓글 목록
+	public List<BoardReplyVO> getListReplyBoard(int start,int end,int board_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		List<BoardReplyVO> list = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "SELECT * FROM (SELECT a.*, rownum rnum FROM (SELECT * FROM board_reply JOIN member USING(mem_num) JOIN member_detail USING(mem_num) WHERE board_num=? ORDER BY re_num DESC)a) WHERE rnum >=? AND rnum <=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, board_num);
+			pstmt.setInt(2, start);
+			pstmt.setInt(3, end);
+			rs = pstmt.executeQuery();
+			list = new ArrayList<BoardReplyVO>();
+			while(rs.next()) {
+				BoardReplyVO reply = new BoardReplyVO();
+				reply.setRe_num(rs.getInt("re_num"));
+				reply.setRe_date(DurationFromNow.getTimeDiffLabel(rs.getString("re_date")));
+				if(rs.getString("re_modifydate")!=null) {
+					reply.setRe_modifydate(DurationFromNow.getTimeDiffLabel(rs.getString("re_modifydate")));
+				}
+				reply.setRe_content(StringUtil.useBrNoHtml(rs.getString("re_content")));
+				reply.setBoard_num(rs.getInt("board_num"));
+				reply.setMem_num(rs.getInt("mem_num"));
+				reply.setMem_nickname(rs.getString("mem_nickname"));
+				
+				list.add(reply);
+			}
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return list;
+	}
 	//댓글 상세(댓글 수정,삭제시 작성자 회원번호 체크 용도로 사용)
+	public BoardReplyVO getReplyBoard(int re_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardReplyVO reply = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "SELECT * FROM board_reply WHERE re_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, re_num);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				reply = new BoardReplyVO();
+				reply.setRe_num(rs.getInt("re_num"));
+				reply.setMem_num(rs.getInt("mem_num"));
+			}			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return reply;
+	}
 	//댓글 수정
+	public void updateReplyBoard(BoardReplyVO reply)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "UPDATE board_reply SET re_content=?,re_modifydate=SYSDATE,re_ip=? WHERE re_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, reply.getRe_content());
+			pstmt.setString(2, reply.getRe_ip());
+			pstmt.setInt(3, reply.getRe_num());
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
 	//댓글 삭제
-
-
+	public void deleteReplyBoard(int re_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "DELETE FROM board_reply WHERE re_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, re_num);
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
 }
--- petmama/src/main/java/kr/board/vo/BoardVO.java
+++ petmama/src/main/java/kr/board/vo/BoardVO.java
@@ -5,7 +5,6 @@ import java.sql.Date;
 public class BoardVO {
 	//board 데이터
 	private int board_num;
-	private int board_type;
 	private String title;
 	private String content;
 	private int hit;
@@ -16,72 +15,106 @@ public class BoardVO {
 	private int mem_num;
 	private String mem_nickname;
 	
+	//member 데이터
+	private String mem_id;
 	
+	//pet 데이터
+	private String pet_photo;
+
 	public int getBoard_num() {
 		return board_num;
 	}
+
 	public void setBoard_num(int board_num) {
 		this.board_num = board_num;
 	}
-	public int getBoard_type() {
-		return board_type;
-	}
-	public void setBoard_type(int board_type) {
-		this.board_type = board_type;
-	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public int getHit() {
 		return hit;
 	}
+
 	public void setHit(int hit) {
 		this.hit = hit;
 	}
+
 	public Date getReg_date() {
 		return reg_date;
 	}
+
 	public void setReg_date(Date reg_date) {
 		this.reg_date = reg_date;
 	}
+
 	public Date getModify_date() {
 		return modify_date;
 	}
+
 	public void setModify_date(Date modify_date) {
 		this.modify_date = modify_date;
 	}
+
 	public String getFilename() {
 		return filename;
 	}
+
 	public void setFilename(String filename) {
 		this.filename = filename;
 	}
+
 	public String getIp() {
 		return ip;
 	}
+
 	public void setIp(String ip) {
 		this.ip = ip;
 	}
+
 	public int getMem_num() {
 		return mem_num;
 	}
+
 	public void setMem_num(int mem_num) {
 		this.mem_num = mem_num;
 	}
+
 	public String getMem_nickname() {
 		return mem_nickname;
 	}
+
 	public void setMem_nickname(String mem_nickname) {
 		this.mem_nickname = mem_nickname;
+	}
+
+	public String getMem_id() {
+		return mem_id;
+	}
+
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+
+	public String getPet_photo() {
+		return pet_photo;
+	}
+
+	public void setPet_photo(String pet_photo) {
+		this.pet_photo = pet_photo;
 	}
 	
 }
