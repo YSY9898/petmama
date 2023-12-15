2bb9e3e530781fb3b2a1080ce53c57518ec75c2f
leesha1223 <leesha1223@naver.com>
2023-12-15 10:48:45 +0900

    원복

--- petmama/src/main/java/kr/customer/action/QnaSearchAction.java
+++ petmama/src/main/java/kr/customer/action/QnaSearchAction.java
@@ -1,61 +1,61 @@
-//package kr.customer.action;
-//
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//
-//import org.codehaus.jackson.map.ObjectMapper;
-//
-//import com.google.gson.Gson;
-//
-//import kr.controller.Action;
-//import kr.qna.dao.QnaDAO;
-//import kr.qna.vo.QnaVO;
-//import kr.util.PageUtilForAjax;
-//
-//public class QnaSearchAction implements Action {
-//	@Override
-//	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-//		String pageNum = request.getParameter("pageNum"); //처음 호출할때 pageNum이 없어서 null일때 1표시
-//		
-//		HttpSession session = request.getSession();
-//		Map<String, String> mapAjax = new HashMap<String, String>();
-//
-//		if(pageNum == null) pageNum = "1";
-//		
-//		String keyfield = request.getParameter("keyfield");
-//		String keyword = request.getParameter("keyword");
-//		
-//		if(keyfield.equals("3")) {
-//			keyword = (String) session.getAttribute("user_id");
-//		}
-//		
-//		QnaDAO dao = QnaDAO.getInstance();
-//		int count = dao.getQnaCount(keyfield, keyword);
-//		
-//		//페이지 처리
-//		PageUtilForAjax page = new PageUtilForAjax(keyfield,keyword,Integer.parseInt(pageNum),count,20,10,"list.do");
-//		
-//		List<QnaVO> list = null;
-//		if(count > 0) {
-//			list = dao.getListQna(page.getStartRow(), page.getEndRow(), keyfield, keyword);
-//		}
-//		
-//		String json = new Gson().toJson(list);
-//		String json2 = new Gson().toJson(page);
-//		
-//		mapAjax.put("result", "success");
-//		mapAjax.put("count", String.valueOf(count));
-//		mapAjax.put("list", json);
-//		mapAjax.put("page", json2);
-//		ObjectMapper mapper = new ObjectMapper();
-//		String ajaxData = mapper.writeValueAsString(mapAjax);
-//		request.setAttribute("ajaxData", ajaxData);
-//
-//		return "/WEB-INF/views/common/ajax_view.jsp";
-//	}
-//}
+package kr.customer.action;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.google.gson.Gson;
+
+import kr.controller.Action;
+import kr.qna.dao.QnaDAO;
+import kr.qna.vo.QnaVO;
+import kr.util.PageUtilForAjax;
+
+public class QnaSearchAction implements Action {
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String pageNum = request.getParameter("pageNum"); //처음 호출할때 pageNum이 없어서 null일때 1표시
+		
+		HttpSession session = request.getSession();
+		Map<String, String> mapAjax = new HashMap<String, String>();
+
+		if(pageNum == null) pageNum = "1";
+		
+		String keyfield = request.getParameter("keyfield");
+		String keyword = request.getParameter("keyword");
+		
+		if(keyfield.equals("3")) {
+			keyword = (String) session.getAttribute("user_id");
+		}
+		
+		QnaDAO dao = QnaDAO.getInstance();
+		int count = dao.getQnaCount(keyfield, keyword);
+		
+		//페이지 처리
+		PageUtilForAjax page = new PageUtilForAjax(keyfield,keyword,Integer.parseInt(pageNum),count,20,10,"list.do");
+		
+		List<QnaVO> list = null;
+		if(count > 0) {
+			list = dao.getListQna(page.getStartRow(), page.getEndRow(), keyfield, keyword);
+		}
+		
+		String json = new Gson().toJson(list);
+		String json2 = new Gson().toJson(page);
+		
+		mapAjax.put("result", "success");
+		mapAjax.put("count", String.valueOf(count));
+		mapAjax.put("list", json);
+		mapAjax.put("page", json2);
+		ObjectMapper mapper = new ObjectMapper();
+		String ajaxData = mapper.writeValueAsString(mapAjax);
+		request.setAttribute("ajaxData", ajaxData);
+
+		return "/WEB-INF/views/common/ajax_view.jsp";
+	}
+}
--- petmama/src/main/java/kr/customer/action/QnaWriteAction.java
+++ petmama/src/main/java/kr/customer/action/QnaWriteAction.java
@@ -1,83 +1,83 @@
-//package kr.customer.action;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//
-//import org.codehaus.jackson.map.ObjectMapper;
-//
-//import com.oreilly.servlet.MultipartRequest;
-//
-//import kr.board.dao.BoardDAO;
-//import kr.board.vo.BoardVO;
-//import kr.controller.Action;
-//import kr.qna.dao.QnaDAO;
-//import kr.qna.vo.QnaVO;
-//import kr.util.FileUtil;
-//
-//public class QnaWriteAction implements Action {
-//
-//	@Override
-//	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-//		HttpSession session = request.getSession();
-//		Map<String, String> mapAjax = new HashMap<String, String>();
-//
-//		Integer user_num = (Integer) session.getAttribute("user_num");
-//		String user_id = (String) session.getAttribute("user_id");
-//		if (user_num == null) {// 로그인이 되지 않은 경우
-//			mapAjax.put("result", "logout");
-//			ObjectMapper mapper = new ObjectMapper();
-//			String ajaxData = mapper.writeValueAsString(mapAjax);
-//			request.setAttribute("ajaxData", ajaxData);
-//
-//			return "/WEB-INF/views/common/ajax_view.jsp";
-//		}
-//
-//		// 로그인이 된 경우
-//		int sis_num = 0;
-//		if (request.getParameter("sis_num") != null) {
-//			sis_num = Integer.parseInt(request.getParameter("sis_num"));
-//		}
-//		String title = request.getParameter("title");
-//		String content = request.getParameter("content");
-//		String hide_yn = request.getParameter("hideYN");
-//		int passwd = 0;
-//		if (hide_yn.equals("Y") && request.getParameter("passwd") != null) {
-//			passwd = Integer.parseInt(request.getParameter("passwd"));
-//		}
-//
-//		System.out.println(sis_num);
-//		System.out.println(title);
-//		System.out.println(content);
-//		System.out.println(passwd);
-//		System.out.println(hide_yn);
-//
-//		QnaVO qna = new QnaVO();
-//		qna.setMem_num(user_num);
-//		qna.setMem_id(user_id);
-//		qna.setSis_num(sis_num);
-//		qna.setTitle(title);
-//		qna.setContent(content);
-//		qna.setPasswd(passwd);
-////		qna.setFilename(null);
-//		qna.setHide_yn(hide_yn);
-//
-//		QnaDAO dao = QnaDAO.getInstance();
-//		boolean insertChk = dao.insertQna(qna);
-//
-//		if (insertChk) {
-//			mapAjax.put("result", "success");
-//		} else {
-//			mapAjax.put("result", "error");
-//		}
-//
-//		ObjectMapper mapper = new ObjectMapper();
-//		String ajaxData = mapper.writeValueAsString(mapAjax);
-//		request.setAttribute("ajaxData", ajaxData);
-//
-//		return "/WEB-INF/views/common/ajax_view.jsp";
-//	}
-//}
+package kr.customer.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.oreilly.servlet.MultipartRequest;
+
+import kr.board.dao.BoardDAO;
+import kr.board.vo.BoardVO;
+import kr.controller.Action;
+import kr.qna.dao.QnaDAO;
+import kr.qna.vo.QnaVO;
+import kr.util.FileUtil;
+
+public class QnaWriteAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		Map<String, String> mapAjax = new HashMap<String, String>();
+
+		Integer user_num = (Integer) session.getAttribute("user_num");
+		String user_id = (String) session.getAttribute("user_id");
+		if (user_num == null) {// 로그인이 되지 않은 경우
+			mapAjax.put("result", "logout");
+			ObjectMapper mapper = new ObjectMapper();
+			String ajaxData = mapper.writeValueAsString(mapAjax);
+			request.setAttribute("ajaxData", ajaxData);
+
+			return "/WEB-INF/views/common/ajax_view.jsp";
+		}
+
+		// 로그인이 된 경우
+		int sis_num = 0;
+		if (request.getParameter("sis_num") != null) {
+			sis_num = Integer.parseInt(request.getParameter("sis_num"));
+		}
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		String hide_yn = request.getParameter("hideYN");
+		int passwd = 0;
+		if (hide_yn.equals("Y") && request.getParameter("passwd") != null) {
+			passwd = Integer.parseInt(request.getParameter("passwd"));
+		}
+
+		System.out.println(sis_num);
+		System.out.println(title);
+		System.out.println(content);
+		System.out.println(passwd);
+		System.out.println(hide_yn);
+
+		QnaVO qna = new QnaVO();
+		qna.setMem_num(user_num);
+		qna.setMem_id(user_id);
+		qna.setSis_num(sis_num);
+		qna.setTitle(title);
+		qna.setContent(content);
+		qna.setPasswd(passwd);
+//		qna.setFilename(null);
+		qna.setHide_yn(hide_yn);
+
+		QnaDAO dao = QnaDAO.getInstance();
+		boolean insertChk = dao.insertQna(qna);
+
+		if (insertChk) {
+			mapAjax.put("result", "success");
+		} else {
+			mapAjax.put("result", "error");
+		}
+
+		ObjectMapper mapper = new ObjectMapper();
+		String ajaxData = mapper.writeValueAsString(mapAjax);
+		request.setAttribute("ajaxData", ajaxData);
+
+		return "/WEB-INF/views/common/ajax_view.jsp";
+	}
+}
